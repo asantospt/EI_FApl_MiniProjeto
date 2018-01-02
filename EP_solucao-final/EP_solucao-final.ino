7a84b876d20317c8857a6776c68f9ee4ffb59737
a-santos <afonsosantos@gmail.com>
2018-01-02 22:04:05 +0000

    Código para Aula 1 comentado
    Ficheiros do EP1+2 arquivados

--- EP_solucao-final/EP_solucao-final.ino
+++ EP_solucao-final/EP_solucao-final.ino
@@ -3,7 +3,7 @@
  * @brief SMART HOME - EP1 + EP2
  * @date 02/01/2018
  * @authors Afonso Santos (nr. 2130653) & Natanael Godinho (nr. 2110634)
- * @state TESTADO! 02/01 - 19.59h    // rever código
+ * @state OK
  */
 
  /** 
@@ -15,38 +15,38 @@
   * Pin 3 -> Sensor ultrassons/ Echo           // EP1
   * Pin 4 -> Buzzer/ +                         // EP1
   * Pin 5 -> LED vermelho                      // EP2
-  * Pin 49 -> Res. 1 kOhm + (LCD/ DC-RS) + Res. 2 kOhm        // Uno 0
-  * Pin 50 -> LCD/ MISO-SD0                                   // Uno 12
-  * Pin 51 -> Res. 1 kOhm + (LCD/ MOSI-SDI) + Res. 2 kOhm     // Uno 11                               
-  * Pin 52 -> Res. 1 kOhm + (LCD/ SCK) + Res. 2 kOhm          // Uno 13
-  * Pin 53 -> Res. 1 kOhm + (LCD/ CS) + Res. 2 kOhm           // Uno 10
+  * Pin 49 -> Res. 1 kOhm + (LCD/ DC-RS) + Res. 2 kOhm + '-'       // Uno 0
+  * Pin 50 -> LCD/ MISO-SD0                                        // Uno 12
+  * Pin 51 -> Res. 1 kOhm + (LCD/ MOSI-SDI) + Res. 2 kOhm + '-'    // Uno 11                               
+  * Pin 52 -> Res. 1 kOhm + (LCD/ SCK) + Res. 2 kOhm + '-'         // Uno 13
+  * Pin 53 -> Res. 1 kOhm + (LCD/ CS) + Res. 2 kOhm + '-'          // Uno 10
   *
   * @Sensor reflexão 'QRE1113'
-  * 1 -> 5 V + R1 (130 kOhm)
+  * 1 -> 5 V + R1 (130 kOhm) + '-'
   * 2 -> GND
-  * 3 -> V_out + R2 (10 kOhm) 
+  * 3 -> V_out + (Arduino/A0) + R2 (10 kOhm) + '-'  
   * 4 -> GND
   *
   * @Sensor ultrassons 'HC-SR04'
   * VCC -> 5 V
   * GND -> GND 
-  * Trig -> Arduino/ Pin 2
-  * Echo -> Arduino/ Pin 3
+  * Trig -> Arduino/Pin 2
+  * Echo -> Arduino/Pin 3
   * 
   * @Buzzer 
-  * + -> Arduino/ Pin 4 + Res. 10 kOhm
+  * + -> Res. 10 kOhm + Arduino/Pin 4
   * - -> GND
   *
-  * @ecrã LCD TFT  TODO:
+  * @ecrã LCD TFT 
   * VCC -> 3.3 V
   * GND -> 0 V
-  * CS ->    Res. 2 kOhm + (CS) + Res. 1 kOhm + Arduino/ Pin 53
-  * DC-RS -> Res. 2 kOhm + (DC-RS) + Res. 1 kOhm + Arduino/ Pin 49
-  * MOSI-SDI ->  Res. 2 kOhm + (MOSI-SDI) + Res. 1 kOhm + Arduino/ Pin 51     // TODO: bem?
-  * MISO-SD0 ->  Arduino/ Pin 50
-  * SCK ->   Res. 2 kOhm + (SCK) + Res. 1 kOhm + Arduino/ Pin 52
-  * LED ->   Res. 10 kOhm + VCC
+  * CS ->       '-' + Res. 2 kOhm + (CS) + Res. 1 kOhm + Arduino/Pin 53
   * RESET -> VCC
+  * DC-RS ->    '-' + Res. 2 kOhm + (DC-RS) + Res. 1 kOhm + Arduino/Pin 49
+  * MOSI-SDI -> '-' + Res. 2 kOhm + (MOSI-SDI) + Res. 1 kOhm + Arduino/Pin 51     
+  * SCK ->      '-' + Res. 2 kOhm + (SCK) + Res. 1 kOhm + Arduino/Pin 52
+  * LED -> Res. 10 kOhm + VCC             // No datasheet do LCD, indicam para ligar diretamente aos 3.3 V
+  * MISO-SD0 -> Arduino/Pin 50
 */
 
 // Bibliotecas extra usadas
@@ -55,81 +55,37 @@
 #include <Adafruit_ILI9341.h>
 
 // LCD: definir os terminais de DC e CS
-#define TFT_DC 49      // TODO: conferir com Natanael, terminal=0 (trig e echo tb estava trocado) 
+#define TFT_DC 49       
 #define TFT_CS 53
 
 // Atribuição dos pinos do Arduino 
+const int PIN_QRE = A0;
 const int PIN_TRIG = 2;
 const int PIN_ECHO = 3;
 const int PIN_BUZZER = 4;
-const int FREQ_BUZZER = 880;
-
-// Valores pré-definidos
-
-// Variáveis globais do programa
-
-
-// criar um objeto tft com indicacao dos terminais DC e CS
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-
-// Valores pré-definidos no enunciado
-/* Distancia Minima para iniciar procedimento de alarme
-60 cm
-Tempo para introduzir código de segurança e desligar alarme
-10 Segundos */
-
-// vel. propagação dos ultrassons no ar = 343 m/s 
-// Formula Utilizada Dcm = deltaT / 58,31 cm/MicroSegundo
-const float VEL_ULTRASSONS_AR = 58.31;  
-
-//Centimetros
-const int DIST_MIN_ALARM = 60;
-//Segundos
-const int TIME_TO_SHUTDOWN = 10;
-
-//Controle de tempos
-//T1: Tempo entre medição de distancia - Milisegundos
-//T2: Tempo até acionamento do alarme - Milisegundos
-const int DELTA_T1 = 1000;
-unsigned long tRef1 = 0;
-
-const int DELTA_T2 = 10000;
-unsigned long tRef2 = 0;
-
-//Alarme accionado
-bool alarm = 0;
-
-//PRINT INTRUSÃO
-bool intrusao = 0;
-
-//Codigo Alarme
-int SECRET = 1234;
-
-//EP2
-// Constantes usadas no programa 
-const int PIN_QRE = A0;
 const int PIN_LED_EP2 = 5;
 
-//bool led = 0;       // TODO: usado? n pode ser zero
-int contagem = 0;
-//Variavel para implementar funcionalidade de apenas uma ocorrencia por reflexão
-//ao entrar no range definido só volta a contar depois de sair do range indicado
-bool toogle = 0;
-
 // Valores pré-definidos no enunciado
-/**
-Valor impar se animal está na rua e valor par se estiver em casa
-*/
+const int FREQ_BUZZER = 880;            // Frequência [Hz]
+const float DIST_MIN_ALARM = 61.0;      // Distância mínima [Centímetros]
+const int DELTA_T2 = 10000;             // 10 s para acionar o alarme [Segundos]
 
-// Valores de referência - Montagem Inicial
-/**
-sensorValueHIGH: 1000
-sensorValueLOW: 500
-Formula para identificar passagem de animal:
-Valor médio:
-(100+500)/2=750
-Tolerancia = 50 + -
-*/
+// Variáveis globais do programa
+const int V_MIN_QRE = 400;          // Valor mínimo definido para identificação de reflexão
+const int V_MAX_QRE = 850;          // Valor máximo definido para identificação de reflexão
+unsigned long tRef2 = 0;                // Varíavel para guardar tempo de referência de contagem dos 10 s
+const int DELTA_T1 = 1000;              // Tempo entre medições de distância [Milisegundos]
+unsigned long tRef1 = 0;                // Varíavel para guardar tempo de referência entre medições de distância
+const float VEL_ULTRASSONS_AR = 58.31;  // Velocidade de propagação dos ultrassons no ar = 343 m/s 
+                                        // Formula Utilizada D[cm] = deltaT / 58,31 [cms/Microsegundos]
+int alarm = 0;       // Varíavel para indicar acionamento do alarme
+int intrusao = 0;    // Varíavel para escrever 1 única vez no ecrã
+int SECRET = 1234;   // Código do Alarme
+int contagem = 0;     // Contador de ocorrências
+int toogle = 0;       // Varíavel para garantir 1 única ocorrência de cada vez que faz 1 leitura dentro do intervalo definido
+
+// Criar um objeto tft com indicacao dos terminais DC e CS
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 // Declaração de funções
 void funcEp1();
@@ -144,118 +100,105 @@ void setup() {
 
   tft.begin();    // inicializar o tft
   tft.fillScreen(ILI9341_BLACK);    // colocar fundo a preto
-  tft.setRotation(2);       // definir orientacao da escrita
+  tft.setRotation(2);               // definir orientacao da escrita
   
   digitalWrite(PIN_TRIG, LOW);  // Ao inicializar o sistema o pin Trig deve estar em modo LOW
   delayMicroseconds(2);
 
   digitalWrite(PIN_LED_EP2, HIGH);
 
-  tone(PIN_BUZZER, FREQ_BUZZER, 500);      // TODO: var p/ tempos buzzer? falta calar o buzzer no Loop
-  delay(500);                  // Acionar o buzzer por 0,5 segundos inicialmente
+  tone(PIN_BUZZER, FREQ_BUZZER, 500);     
+  delay(500);                       // Acionar o buzzer por 0,5 segundos inicialmente
   noTone(PIN_BUZZER);                    
 
-  delay(1500);                 // Para completar os 2 s em que o LED do EP2 deve estar ligado no arranque
-  digitalWrite(PIN_LED_EP2, LOW);   // TODO: testar se LED só acende 2 segs
+  delay(1500);                      // Para completar os 2 s em que o LED do EP2 deve estar ligado no arranque
+  digitalWrite(PIN_LED_EP2, LOW);   
 }
 
 void loop() {
   funcEp1();        // Função com funcionalidades do EP1
   funcEp2();        // Função com funcionalidades do EP2
 
-  //para garantir bom funcionamento do sensor ultrasons
+  // Para garantir bom funcionamento do sensor ultrasons
   delay(100);
 }
 
-void funcEp1(){
-  //Contar milisegundos desde o arranque do sistema
-  float instanteAtual = millis();
+void funcEp1() {
+  float distance = 0.0;
+  float instanteAtual = millis();       // Contar milisegundos desde o arranque do sistema
+  long duration = 0;      // Registar a duração do pulso do echo [Microsegundos]
+  int secret = 0;
 
-  //Registar a duração do pulso do echo - Microsegundos
-  long duration;
-
-  //Tratar dos Pinos do sensor Ultrasons
-  digitalWrite(PIN_TRIG, HIGH);
+  digitalWrite(PIN_TRIG, HIGH);     // Tratar dos Pinos do sensor Ultrasons
   delayMicroseconds(10);
   digitalWrite(PIN_TRIG, LOW); 
   
-  // Ler distancia a cada segundo
-  if ( (instanteAtual - tRef1) >= DELTA_T1 ){
+  if ((instanteAtual - tRef1) >= DELTA_T1 ) {        // Ler distancia a cada segundo
     duration = pulseIn(PIN_ECHO, HIGH);
-    float distance = ((float)duration / VEL_ULTRASSONS_AR);  
+    distance = ((float)duration / VEL_ULTRASSONS_AR);  
       
-      if (alarm == 0 && distance < 61){
-        Serial.print(distance); 
-        Serial.println(" cm");
-        Serial.println("Insira código de Segurança");
-        tRef1 = instanteAtual;
-        tRef2 = instanteAtual;
-        alarm = 1;
-        intrusao = 0;
-        //escrever para o ecrã
-        tft.fillScreen(ILI9341_YELLOW);
-        tft.setCursor(0, 0);
-        tft.setTextColor(ILI9341_RED); 
-        tft.setTextSize(2);
-        tft.println("Insira Codigo Seguranca");
-       }
+    if (alarm == 0 && distance < DIST_MIN_ALARM) {
+      Serial.print(distance); 
+      Serial.println(" cm");
+      Serial.println("Insira codigo de Seguranca ");
+      tRef1 = instanteAtual;
+      tRef2 = instanteAtual;
+      alarm = 1;
+      intrusao = 0;         // Escrever para o ecrã 1 única vez
+      tft.fillScreen(ILI9341_YELLOW);
+      tft.setCursor(0, 0);
+      tft.setTextColor(ILI9341_RED); 
+      tft.setTextSize(2);
+      tft.println("Insira codigo Seguranca ");
+    }
   }
 
-  int secret;
-  //receber texto enviado pela consola da porta serie
-  secret = Serial.parseInt();
-
+  secret = Serial.parseInt();       // Receber texto enviado pela consola da porta série
   if (alarm == 1 && SECRET == secret ){
-    //tone(PIN_BUZZER, FREQ_BUZZER, 1);
-
     noTone(PIN_BUZZER);
     alarm = 0;
-    //escrever no ecrã
-    tft.fillScreen(ILI9341_GREEN);
+    tft.fillScreen(ILI9341_GREEN);      // Escrever para o ecrã 
     tft.setCursor(0, 0);
     tft.setTextColor(ILI9341_BLACK); 
     tft.setTextSize(2);
     tft.println("OK");
   }
 
-  if (alarm == 1 && (instanteAtual - tRef2) >= DELTA_T2 ){
-    if(intrusao == 0){
-      //escrever no ecrã
-      tft.setCursor(0, 0);
+  if (alarm == 1 && (instanteAtual - tRef2) >= DELTA_T2) {
+    if (intrusao == 0) {
+      tft.setCursor(0, 0);        // Escrever para o ecrã 
       tft.fillScreen(ILI9341_RED); 
       tft.setTextColor(ILI9341_WHITE); 
       tft.setTextSize(3);
-      tft.println("INTRUSAO");
+      tft.println("INTRUSAO!");
       
       Serial.println("INTRUSÃO!");
       intrusao = 1;
     }
-    tone(PIN_BUZZER, FREQ_BUZZER, 0);
+    tone(PIN_BUZZER, FREQ_BUZZER, 0);       // 0, tocar indefinidamente
   }
 }
 
-void funcEp2 (){
-    //Ler valor do Sensor
-  int sensorValue = analogRead(PIN_QRE);
-  //degug
-  Serial.println(sensorValue);
-  //debug
-  Serial.println(contagem);
+void funcEp2 () {
+  int sensorValue = 0;
 
-  if(sensorValue > 400 && sensorValue < 850){
-    if(toogle==0){
-        contagem = contagem + 1;
-        toogle = 1;
+  sensorValue = analogRead(PIN_QRE);        // Ler valor do Sensor
+  Serial.println(sensorValue);      //degug
+  Serial.println(contagem);         //debug
+
+  if (sensorValue > V_MIN_QRE && sensorValue < V_MAX_QRE) {
+    if (toogle == 0) {
+      contagem = contagem + 1;
+      toogle = 1;
     }
-  }else{
-      toogle = 0;
-    }
+  } else {
+    toogle = 0;
+  }
     
-  if ( (contagem & 1) == 0) {
-     //Por o led a High
-     digitalWrite(PIN_LED_EP2, LOW);    
-  }else{
-    //Por o led a low
-    digitalWrite(PIN_LED_EP2, HIGH);
-    }
+  // Se o resto da divisão for zero, LED é apagado
+  if ((contagem & 1) == 0) {
+    digitalWrite(PIN_LED_EP2, LOW);        // Por o LED a Low
+  } else {
+    digitalWrite(PIN_LED_EP2, HIGH);       // Por o LED a High
+  }
 }
