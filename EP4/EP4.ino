18633075fa35971ca0cef58be53159caea721209
a-santos <afonsosantos@gmail.com>
2018-01-06 18:14:48 +0000

    EP - alteração da função da reta

--- EP4/EP4.ino
+++ EP4/EP4.ino
@@ -24,11 +24,11 @@
 
 // Atribuição dos pinos do Arduino 
 const int PIN_LDR = A2; 
-const int PIN_LED_EP4 = 9;
+const int PIN_LED_EP4 = 5;      // verde, Mega 9
 
 // Valores pré-definidos no enunciado 
 const int MAX_BRILHO_MADRUGADA = 128;     // Metade do brilho máximo = 127.5
-const int DELTA_T3 = 10000;             // 10 s para simular as 04:00h da madrugada [Segundos]
+const int DELTA_T3 = 10000;               // 10 s para simular as 04:00h da madrugada [Segundos]  
 
 // Variáveis globais do programa
 const int MIN_PERC_BRILHO = 0;        // Brilho mínimo 
@@ -36,7 +36,9 @@ const int MAX_PERC_BRILHO = 255;      // Brilho máximo
 const float VALOR_MIN_LUZ = 2.00;     // valor min. de luminosidade lido (tapado) = 2.00    TODO:
 const float VALOR_MAX_LUZ = 4.50;     // valor max. de luminosidade lido (a descoberto) = 4.50 TODO: test leitura
 const int TEMPO_MADRUGADA = 10000;    // Simulação da 04:00 da madrugada. 10 s [Milissegundos]
+unsigned long instanteAtual_Ep4 = 0;
 unsigned long tRef3 = 0;              // Varíavel para guardar tempo de referência de contagem dos 10 s
+boolean isMadrugada = false;             
 
 // Declaração de funções
 void funcao_Ep4(int maxBrilho);
@@ -53,23 +55,29 @@ void setup() {
 }
 
 void loop() {
-  unsigned long instanteAtual = 0;
-  boolean madrugada = false; 
+  instanteAtual_Ep4 = millis();       // Contar milisegundos desde o arranque do sistema
 
-  instanteAtual = millis();       // Contar milisegundos desde o arranque do sistema
-  
-  if ((instanteAtual - tRef3) >= DELTA_T3) {        
-    // Já passaram os 10 s desde o arranque do programa, significa que já é madrugada
-    tRef3 = instanteAtual;
-    //madrugada = true;
-    Serial.println("** passaram 10 s desde arranque. F-Madrugada!! ");
-    funcao_Ep4(MAX_PERC_BRILHO);
-  } else {
-    //madrugada = false;
+  /*analogWrite(PIN_LED_EP4, 50); 
+  delay(500);
+  analogWrite(PIN_LED_EP4, 100); 
+  delay(500);*/
+
+  if (((instanteAtual_Ep4 - tRef3) >= DELTA_T3) && isMadrugada == false) {        
+    // Já passaram os 10 s desde o arranque do programa, significa que já é 'madrugada'
+    tRef3 = instanteAtual_Ep4;
+    isMadrugada = true;
+      //Serial.println("** passaram 10 s desde arranque. F-Madrugada!!");
     funcao_Ep4(MAX_BRILHO_MADRUGADA);
+  } else {
+    funcao_Ep4(MAX_PERC_BRILHO);
   }
 }
 
+/**
+ * @brief Recebe o brilho máximo a aplicar, lê luminosidade existente e atua sobre o brilho de um LED
+ * @params Valor máximo do brilho, em função da hora
+ * @return -
+ */
 void funcao_Ep4(int maxBrilho) {
   int sensorValueLDR = 0;
   float voltageLDR = 0.0;
@@ -80,28 +88,23 @@ void funcao_Ep4(int maxBrilho) {
   Serial.println(sensorValueLDR);     // debug
 
   voltageLDR = sensorValueLDR * (5.0 / 1023.0);     // Conversão da leitura analógica (0-1023) para uma voltagem (0-5 V) 
-  Serial.println(voltageLDR);      // debug
+  //Serial.println(voltageLDR);      // debug
 
   /* 
-  x = luminosidade LDR
-  y = brilho LED
-  Eq. da reta: y = y0 + m(x - x0)
-  Declive: m = (y1 - y0) / (x1 - x0) */
-  declive = (float)(maxBrilho - MIN_PERC_BRILHO) / (VALOR_MAX_LUZ - VALOR_MIN_LUZ);
-                  // maxBrilho é o valor máx. para a luminosidade, seja 
-  brilhoLED = 0 + declive * (sensorValueLDR - VALOR_MIN_LUZ);
+  x = luminosidade LDR (VALOR_MIN_LUZ, VALOR_MAX_LUZ)
+  y = brilho LED (MIN_PERC_BRILHO, MAX_PERC_BRILHO)
+  Declive: m = (y1 - y0) / (x1 - x0) 
+  Equação da reta: y = y0 + m(x - x0)   */
+  /*declive = (float)(maxBrilho - MIN_PERC_BRILHO) / (VALOR_MAX_LUZ - VALOR_MIN_LUZ);
+       // maxBrilho é o valor máx. da luminosidade, em função da hora do dia
+  brilhoLED = (int) 0 + declive * (sensorValueLDR - VALOR_MIN_LUZ);*/
 
-  analogWrite(PIN_LED_EP4, brilhoLED);
+  brilhoLED = map(voltageLDR, VALOR_MIN_LUZ, VALOR_MAX_LUZ, MIN_PERC_BRILHO, MAX_PERC_BRILHO);
+  if(brilhoLED > MIN_PERC_BRILHO || brilhoLED < MAX_PERC_BRILHO) {
+    analogWrite(PIN_LED_EP4, brilhoLED);
+  }  
 
-  Serial.println("** luz a metade!");     // debug
-  analogWrite(PIN_LED_EP4, MAX_BRILHO_MADRUGADA); 
-}
-
-/**
- * @descr 
- * @params 
- * @return 
- */
-void lerLuminosidade() {  
-
+    //Serial.println(maxBrilho);     // debug
+    //Serial.println("** luz a metade!");     // debug
+    //analogWrite(PIN_LED_EP4, MAX_BRILHO_MADRUGADA); 
 }
--- EP5/EP5.ino
+++ EP5/EP5.ino
@@ -20,27 +20,27 @@
 
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo myservo;            // Criação de 1 objeto para controlar o Servo
 
 // Atribuição dos pinos do Arduino
 const int PIN_SERVO = 1;
 
+// Valores pré-definidos no enunciado 
+
 // Valores pré-definidos 
-const int MAX_ANG_SERVO = 170;
 const int MIN_ANG_SERVO = 10;
-const int T_LIMIAR_1 = 15;              // Temperatura considerado como 'Frio'
-const int T_LIMIAR_2 = 30;              // Temperatura considerado como 'Calor'
-const float T_LIMIAR_LUZ = 80.0;        // Valor para incidência de luz solar
+const int MAX_ANG_SERVO = 170;
+const float T_LIMIAR_1 = 15.0;              // Temperatura considerado como 'Frio'
+const float T_LIMIAR_2 = 30.0;              // Temperatura considerado como 'Calor'
+const float T_LIMIAR_LUZ = 80.0;            // Valor para incidência de luz solar
             // TODO: medir valores
-const int DELTA_T1 = 10000;
+//const int DELTA_T1 = 10000;
 const int ANG_ESTORES_ABERTOS = 30; 
-const int ANG_ESTORES_FECHADOS = 120; 
 const int ANG_ESTORES_INT = 75;
+const int ANG_ESTORES_FECHADOS = 120; 
 
 // Variáveis globais do programa
-unsigned long tRef = 0;
-int pos = 0;                 // variável que guarda a posição do servo
+int posServo = 0;                 // Variável que guarda a posição do servo
 
 // Declaração de funções
 void funcao_Ep5(float tempEp3);
@@ -63,34 +63,27 @@ void loop() {
   float tempEp3 = 0.0;
   float luminosidade_ep4 = 0.0;
 
-  tempEp3 = 9.0;
-  funcao_Ep5(tempEp3);
-
-  // TODO: verificar alterações a cada 10 segs.
-  /*instanteAtual = millis();
-  if ((instanteAtual - tRef) >= DELTA_T1) {   
-      //ação a executar a cd 10 segs
-      tRef = instanteAtual;
-  }*/
+  /*tempEp3 = 9.0;
+  funcao_Ep5(tempEp3);*/
 
   // ######################## testar - sweep
   // goes from 0 degrees to 170 degrees in steps of 1 degree
-  for (pos = MIN_ANG_SERVO; pos <= MAX_ANG_SERVO; pos += 1) { 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    Serial.println(pos); 
+  for (posServo = MIN_ANG_SERVO; posServo <= MAX_ANG_SERVO; posServo += 1) { 
+    myservo.write(posServo);              // tell servo to go to position in variable 'posServo'
+    Serial.println(posServo); 
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = MAX_ANG_SERVO; pos >= MIN_ANG_SERVO; pos -= 1) { // goes from 170 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    Serial.println(pos); 
+  for (posServo = MAX_ANG_SERVO; posServo >= MIN_ANG_SERVO; posServo -= 1) { // goes from 170 degrees to 0 degrees
+    myservo.write(posServo);              // tell servo to go to position in variable 'posServo'
+    Serial.println(posServo); 
     delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
 
 void funcao_Ep5(float tempEp3) {
   // debug - valores para teste
-  /*tempEp3 = 9;           // TODO: receber valores do EP3, mas validá-los antes (dentro de 1 intervalo aceitável)
-  luminosidade_ep4 = 90.0;
+             // TODO: receber valores do EP3, mas validá-los antes (dentro de 1 intervalo aceitável)
+  float luminosidade_ep4 = 90.0;
 
   // Se estiver frio
   if ((tempEp3 < T_LIMIAR_1) && (luminosidade_ep4 > T_LIMIAR_LUZ))  {
@@ -115,7 +108,7 @@ void funcao_Ep5(float tempEp3) {
     Serial.print("temp entre valores = Estores em posição intermédia"); // debug
     Serial.println(ANG_ESTORES_FECHADOS);     
     delay(15);
-  }*/
+  }
 
-  // Em ambiente real, verificação poderia ser a cada 15 mins 
+  // Nota: em ambiente real, verificação poderia ser a cada 15 minutos
 }
