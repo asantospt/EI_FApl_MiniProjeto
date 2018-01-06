53586d46a93304e9dbe68f12335183000cadbfbd
Natanael Godinho <2110634@my.ipleiria.pt>
2018-01-06 19:08:00 +0000

    EP4 correções

--- EP4/EP4.ino
+++ EP4/EP4.ino
@@ -24,7 +24,7 @@
 
 // Atribuição dos pinos do Arduino 
 const int PIN_LDR = A2; 
-const int PIN_LED_EP4 = 5;      // verde, Mega 9
+const int PIN_LED_EP4 = 4;      // verde, Mega 9
 
 // Valores pré-definidos no enunciado 
 const int MAX_BRILHO_MADRUGADA = 128;     // Metade do brilho máximo = 127.5
@@ -33,8 +33,8 @@ const int DELTA_T3 = 10000;               // 10 s para simular as 04:00h da madr
 // Variáveis globais do programa
 const int MIN_PERC_BRILHO = 0;        // Brilho mínimo 
 const int MAX_PERC_BRILHO = 255;      // Brilho máximo 
-const float VALOR_MIN_LUZ = 2.00;     // valor min. de luminosidade lido (tapado) = 2.00    TODO:
-const float VALOR_MAX_LUZ = 4.50;     // valor max. de luminosidade lido (a descoberto) = 4.50 TODO: test leitura
+const float VALOR_MIN_LUZ = 3.5;     // valor min. de luminosidade lido (tapado) = 2.00    TODO:
+const float VALOR_MAX_LUZ = 4.5;     // valor max. de luminosidade lido (a descoberto) = 4.50 TODO: test leitura
 const int TEMPO_MADRUGADA = 10000;    // Simulação da 04:00 da madrugada. 10 s [Milissegundos]
 unsigned long instanteAtual_Ep4 = 0;
 unsigned long tRef3 = 0;              // Varíavel para guardar tempo de referência de contagem dos 10 s
@@ -51,22 +51,23 @@ void setup() {
   // Acender o LED 2 segs. para teste
   digitalWrite(PIN_LED_EP4, HIGH);   
   delay(2000);                  
-  digitalWrite(PIN_LED_EP4, LOW);     
+  digitalWrite(PIN_LED_EP4, LOW);  
+  //delay(2000);   
 }
 
 void loop() {
   instanteAtual_Ep4 = millis();       // Contar milisegundos desde o arranque do sistema
 
-  /*analogWrite(PIN_LED_EP4, 50); 
-  delay(500);
-  analogWrite(PIN_LED_EP4, 100); 
-  delay(500);*/
+  //analogWrite(PIN_LED_EP4, 50); 
+  //delay(500);
+  //analogWrite(PIN_LED_EP4, 100); 
+  //delay(500);
 
   if (((instanteAtual_Ep4 - tRef3) >= DELTA_T3) && isMadrugada == false) {        
     // Já passaram os 10 s desde o arranque do programa, significa que já é 'madrugada'
     tRef3 = instanteAtual_Ep4;
     isMadrugada = true;
-      //Serial.println("** passaram 10 s desde arranque. F-Madrugada!!");
+      Serial.println("** passaram 10 s desde arranque. F-Madrugada!!");
     funcao_Ep4(MAX_BRILHO_MADRUGADA);
   } else {
     funcao_Ep4(MAX_PERC_BRILHO);
@@ -79,16 +80,17 @@ void loop() {
  * @return -
  */
 void funcao_Ep4(int maxBrilho) {
+  int m = maxBrilho;
   int sensorValueLDR = 0;
   float voltageLDR = 0.0;
   float declive = 0.0;
   int brilhoLED = 0;
 
   sensorValueLDR = analogRead(PIN_LDR);
-  Serial.println(sensorValueLDR);     // debug
+  //Serial.println(sensorValueLDR);     // debug
 
   voltageLDR = sensorValueLDR * (5.0 / 1023.0);     // Conversão da leitura analógica (0-1023) para uma voltagem (0-5 V) 
-  //Serial.println(voltageLDR);      // debug
+  Serial.println(voltageLDR);      // debug
 
   /* 
   x = luminosidade LDR (VALOR_MIN_LUZ, VALOR_MAX_LUZ)
@@ -99,8 +101,8 @@ void funcao_Ep4(int maxBrilho) {
        // maxBrilho é o valor máx. da luminosidade, em função da hora do dia
   brilhoLED = (int) 0 + declive * (sensorValueLDR - VALOR_MIN_LUZ);*/
 
-  brilhoLED = map(voltageLDR, VALOR_MIN_LUZ, VALOR_MAX_LUZ, MIN_PERC_BRILHO, MAX_PERC_BRILHO);
-  if(brilhoLED > MIN_PERC_BRILHO || brilhoLED < MAX_PERC_BRILHO) {
+  brilhoLED = map(voltageLDR, VALOR_MIN_LUZ, VALOR_MAX_LUZ, MIN_PERC_BRILHO, m);
+  if(brilhoLED > MIN_PERC_BRILHO || brilhoLED < m) {
     analogWrite(PIN_LED_EP4, brilhoLED);
   }  
 
